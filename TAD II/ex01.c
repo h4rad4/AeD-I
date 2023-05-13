cd2cdf146039e5ae866a60999bd5f39df4505122
h4rad4 <williamharad@outlook.com>
2023-05-13 19:19:31 -0400

    Atividade 13/05/2023

---
+++ TAD II/ex01.c
@@ -0,0 +1,15 @@
+/*
+---------------------------------------------------------------------------------------------
+Desenvolva um TAD que represente um cubo. Inclua as funções de inicializações necessárias e 
+as operações que retornem os tamanhos de cada lado, a sua área e o seu volume.
+---------------------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
