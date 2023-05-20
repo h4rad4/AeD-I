97a0eb598d717ab48abaa0acad819451dfddd17c
h4rad4 <99924347+h4rad4@users.noreply.github.com>
2023-05-20 19:22:39 -0300

    Atividade 12.05.2023

--- TAD II/ex01/header.h
+++ TAD II/ex01/header.h
@@ -2,15 +2,12 @@
 #define MAIN_H
 
 typedef struct {
-    int real;
-    int img;
-} Complexo;
+    double lado;
+} Cubo;
 
-void *criarComplexo(int real, int img);
-void destruirComplexo(Complexo *complexo);
-Complexo *somarComplexos(Complexo *z1, Complexo *z2);
-Complexo *subtrairComplexos(Complexo *z1, Complexo *z2);
-Complexo *multiplicarComplexos(Complexo *z1, Complexo *z2);
-Complexo *dividirComplexos(Complexo *z1, Complexo *z2);
+void dadosCubo(Cubo *cubo);
+double cuboLado(Cubo *cubo);
+double cuboArea(Cubo *cubo);
+double cuboVolume(Cubo *cubo);
 
 #endif
