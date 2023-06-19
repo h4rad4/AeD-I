9e993fc3b96e857b437457dc7261520dd8fd51dc
William Massami <99924347+h4rad4@users.noreply.github.com>
2023-06-19 02:03:36 -0400

    Add files via upload

--- Recursividade/TorreDeHanoi/Recursivo.c
+++ Recursividade/TorreDeHanoi/Recursivo.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
-#include <iostream>
 
-int main(int argc, char const *argv[])
-{
-    int discos = 3;
-
-    moveDisco(discos, "A", "C", "B");
-    // function(qtd, "origem", "destino", "auxiliar");
-
-    return 0;
-}
-z
-void moveDisco(int n, char ori, char dest, char aux)
+void MoveTorre(int n, char origem, char destino, char auxiliar)
 {
     if (n == 1)
-        cout<<"Mover disco"
+    {
+        MoveDisco(origem, destino);
+    }
     else
     {
-
+        MoveTorre(n - 1, origem, auxiliar, destino);
+        MoveDisco(origem, destino);
+        MoveTorre(n - 1, auxiliar, destino, origem);
     }
+}
+
+void MoveDisco(char origem, char destino)
+{
+    printf("Mova o disco de %c para %c\n", origem, destino);
+}
+
+int main()
+{
+    int n = 4;
+    char origem = 'A';
+    char destino = 'C';
+    char auxiliar = 'B';
+
+    MoveTorre(n, origem, destino, auxiliar);
+
+    return 0;
 }
