0ce6455a5ffadfbe2428116b708faf0cf375f59a
h4rad4 <williamharad@outlook.com>
2023-07-01 12:41:11 -0400

    .

--- Filas e pilhas II/fun.c
+++ Filas e pilhas II/fun.c
@@ -3,98 +3,133 @@
 #include <string.h>
 #include "main.h"
 
-// Função para criar uma expressão vazia
+int isOperador(char c)
+{
+    return (c == '+' || c == '-' || c == '*' || c == '/'); // Verifica se o caractere é um operador
+}
+
+int getPrioridade(char c)
+{
+    if (c == '+' || c == '-')
+        return 1; // Retorna a prioridade 1 para os operadores + e -
+    else if (c == '*' || c == '/')
+        return 2; // Retorna a prioridade 2 para os operadores * e /
+    return 0;     // Retorna 0 para outros caracteres (números ou outros)
+}
+
+void inverter_string(char *str)
+{
+    int tamanho = strlen(str);
+    for (int i = 0; i < tamanho / 2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[tamanho - 1 - i];
+        str[tamanho - 1 - i] = temp;
+    }
+    // Inverte uma string trocando os caracteres da posição inicial com a posição final
+}
+
 Expressao criar_expressao()
 {
     Expressao expressao;
-    expressao.tamanho = 0; // Inicializa o tamanho da expressão como zero
-    return expressao;
+    expressao.expressao[0] = '\0';
+    expressao.tamanho = 0;
+    return expressao; // Cria uma expressão vazia
 }
 
-// Função para apagar o conteúdo de uma expressão existente
 void apagar_expressao(Expressao *expressao)
 {
-    expressao->tamanho = 0;         // Define o tamanho da expressão como zero
-    expressao->expressao[0] = '\0'; // Define a expressão como uma string vazia
+    expressao->expressao[0] = '\0';
+    expressao->tamanho = 0; // Apaga uma expressão, definindo seu tamanho como zero e limpando o conteúdo
 }
 
-// Função para imprimir o conteúdo de uma expressão
 void imprimir_expressao(const Expressao *expressao)
 {
-    printf("Expressao: %s\n", expressao->expressao + 1); // Imprime a expressão
+    printf("Expressao: %s\n", expressao->expressao); // Imprime a expressão
 }
 
-// Função para adicionar um elemento (operando ou operador) à expressão
 void adicionar_elemento(Expressao *expressao, const char *elemento)
 {
     int tamanho_atual = expressao->tamanho;
-    int novo_tamanho = tamanho_atual + strlen(elemento) + 2; // Calcula o novo tamanho da expressão
+    int novo_tamanho = tamanho_atual + strlen(elemento) + 2; // Calcula o novo tamanho considerando o elemento e os espaços extras
 
-    strcat(expressao->expressao, " ");      // Adiciona um espaço em branco antes do novo elemento
-    strcat(expressao->expressao, elemento); // Concatena o novo elemento à expressão existente
-    expressao->tamanho = novo_tamanho - 1;  // Atualiza o tamanho da expressão, descontando o caractere nulo
-}
-
-// Função para converter uma expressão posfixa em uma expressão prefixa
-void converter_posfixa_para_prefixa(const Expressao *expressao_posfixa, Expressao *expressao_prefixa)
-{
-    char operadores[] = "+-*/";
-    char pilha[200][200];
-    int topo = -1;
-
-    char expressao_posfixa_temp[200];
-    strcpy(expressao_posfixa_temp, expressao_posfixa->expressao + 1); // Copia a expressão posfixa, ignorando o primeiro caractere nulo
-
-    char *token = strtok(expressao_posfixa_temp, " "); // Divide a expressão em elementos separados por espaços em branco
-    while (token != NULL)
+    if (novo_tamanho <= 200) // Verifica se há espaço suficiente para adicionar o elemento
     {
-        if (strchr(operadores, token[0]) == NULL) // Se for um operando
-        {
-            adicionar_elemento(expressao_prefixa, token); // Adiciona o operando à expressão prefixa
-        }
-        else // Se for um operador
-        {
-            char operando1[200], operando2[200];
-            strcpy(operando1, pilha[topo]); // Obtém o primeiro operando da pilha
-            topo--;
-            strcpy(operando2, pilha[topo]); // Obtém o segundo operando da pilha
-            topo--;
-
-            char expressao_prefixa_temp[200];
-            sprintf(expressao_prefixa_temp, "%s %s %s", token, operando2, operando1); // Cria uma expressão prefixa temporária com o operador e operandos na ordem correta
-            adicionar_elemento(expressao_prefixa, expressao_prefixa_temp); // Adiciona a expressão prefixa temporária à expressão prefixa final
-
-            topo++;
-            strcpy(pilha[topo], expressao_prefixa_temp); // Adiciona a expressão prefixa temporária à pilha
-        }
-        token = strtok(NULL, " "); // Move para o próximo token
+        strcat(expressao->expressao, " ");      // Adiciona um espaço em branco antes do elemento
+        strcat(expressao->expressao, elemento); // Adiciona o elemento à expressão
+        expressao->tamanho = novo_tamanho - 1;  // Atualiza o tamanho, excluindo o caractere nulo
+    }
+    else
+    {
+        printf("Erro: Expressao excedeu o tamanho maximo\n"); // Imprime uma mensagem de erro se a expressão exceder o tamanho máximo
     }
 }
 
+void converter_posfixa_para_prefixa(const Expressao *expressao_posfixa, Expressao *expressao_prefixa)
+{
+    char pilha[200][200];
+    int topo = -1;
 
-// Função para avaliar o resultado de uma expressão posfixa
-float avaliar_expressao(const Expressao *expressao)
+    char *expressao_posfixa_temp = strdup(expressao_posfixa->expressao); // Cria uma cópia temporária da expressão posfixa
+    char *termo = strtok(expressao_posfixa_temp, " ");                   // Divide a expressão em termos separados por espaços
+    while (termo != NULL)
+    {
+        if (isOperador(termo[0])) // Se o termo for um operador
+        {
+            while (topo >= 0 && isOperador(pilha[topo][0]) && getPrioridade(pilha[topo][0]) >= getPrioridade(termo[0]))
+            {
+                // Enquanto houver operadores no topo da pilha com prioridade maior ou igual ao termo atual
+                adicionar_elemento(expressao_prefixa, pilha[topo]); // Adiciona o operador no topo da pilha à expressão prefixa
+                topo--;                                             // Remove o operador do topo da pilha
+            }
+            strcpy(pilha[++topo], termo); // Adiciona o termo atual à pilha
+        }
+        else // Se o termo for um número
+        {
+            adicionar_elemento(expressao_prefixa, termo); // Adiciona o número à expressão prefixa
+        }
+        termo = strtok(NULL, " "); // Avança para o próximo termo
+    }
+
+    while (topo >= 0)
+    {
+        adicionar_elemento(expressao_prefixa, pilha[topo]); // Adiciona os operadores restantes da pilha à expressão prefixa
+        topo--;
+    }
+
+    inverter_string(expressao_prefixa->expressao); // Inverte a expressão prefixa para a ordem correta
+
+    free(expressao_posfixa_temp); // Libera a memória alocada para a expressão posfixa temporária
+}
+
+float resultado_expressao(const Expressao *expressao)
 {
     char operadores[] = "+-*/";
     float pilha[200];
     int topo = -1;
 
     char expressao_temp[200];
-    strcpy(expressao_temp, expressao->expressao); // Copia a expressão posfixa
+    strcpy(expressao_temp, expressao->expressao);
 
-    char *token = strtok(expressao_temp, " "); // Divide a expressão em elementos separados por espaços em branco
-    while (token != NULL)
+    char *termo = strtok(expressao_temp, " "); // Divide a expressão em termos separados por espaços
+    while (termo != NULL)
     {
-        if (strchr(operadores, token[0]) == NULL) // Se for um operando
+        if (strchr(operadores, termo[0]) == NULL) // Se o termo não for um operador
         {
-            pilha[++topo] = atof(token); // Converte o operando para float e adiciona na pilha
+            pilha[++topo] = atof(termo); // Converte o termo em um número e adiciona à pilha
         }
-        else // Se for um operador
+        else // Se o termo for um operador
         {
-            float operando2 = pilha[topo--]; // Obtém o segundo operando da pilha
-            float operando1 = pilha[topo--]; // Obtém o primeiro operando da pilha
+            if (topo < 1)
+            {
+                printf("Erro: Expressao invalida\n"); // Imprime uma mensagem de erro se a expressão for inválida
+                return 0.0;
+            }
+
+            float operando2 = pilha[topo--]; // Remove o último operando da pilha
+            float operando1 = pilha[topo--]; // Remove o penúltimo operando da pilha
             float resultado;
-            switch (token[0]) // Realiza a operação correspondente ao operador
+            switch (termo[0]) // Executa a operação com base no operador
             {
             case '+':
                 resultado = operando1 + operando2;
@@ -109,14 +144,55 @@ float avaliar_expressao(const Expressao *expressao)
                 resultado = operando1 / operando2;
                 break;
             default:
-                printf("Erro: Operador invalido\n");
+                printf("Erro: Operador invalido\n"); // Imprime uma mensagem de erro se o operador for inválido
                 resultado = 0.0;
                 break;
             }
-            pilha[++topo] = resultado; // Adiciona o resultado da operação na pilha
+            pilha[++topo] = resultado; // Adiciona o resultado da operação à pilha
         }
-        token = strtok(NULL, " "); // Move para o próximo token
+        termo = strtok(NULL, " "); // Avança para o próximo termo
     }
 
-    return pilha[topo]; // Retorna o resultado final da expressão posfixa
+    if (topo != 0)
+    {
+        printf("Erro: Expressao invalida\n"); // Imprime uma mensagem de erro se a expressão for inválida
+        return 0.0;
+    }
+
+    return pilha[topo]; // Retorna o resultado final da expressão
+}
+
+void converter_prefixa_para_posfixa(const Expressao *expressao_prefixa, Expressao *expressao_posfixa)
+{
+    char pilha[200][200];
+    int topo = -1;
+
+    char *expressao_prefixa_temp = strdup(expressao_prefixa->expressao); // Cria uma cópia temporária da expressão prefixa
+    char *termo = strtok(expressao_prefixa_temp, " ");                   // Divide a expressão em termos separados por espaços
+    while (termo != NULL)
+    {
+        if (isOperador(termo[0])) // Se o termo for um operador
+        {
+            while (topo >= 0 && isOperador(pilha[topo][0]) && getPrioridade(pilha[topo][0]) >= getPrioridade(termo[0]))
+            {
+                // Enquanto houver operadores no topo da pilha com prioridade maior ou igual ao termo atual
+                adicionar_elemento(expressao_posfixa, pilha[topo]); // Adiciona o operador no topo da pilha à expressão posfixa
+                topo--;                                             // Remove o operador do topo da pilha
+            }
+            strcpy(pilha[++topo], termo); // Adiciona o termo atual à pilha
+        }
+        else // Se o termo for um número
+        {
+            adicionar_elemento(expressao_posfixa, termo); // Adiciona o número à expressão posfixa
+        }
+        termo = strtok(NULL, " "); // Avança para o próximo termo
+    }
+
+    while (topo >= 0)
+    {
+        adicionar_elemento(expressao_posfixa, pilha[topo]); // Adiciona os operadores restantes da pilha à expressão posfixa
+        topo--;
+    }
+
+    free(expressao_prefixa_temp); // Libera a memória alocada para a expressão prefixa temporária
 }
--- Filas e pilhas II/main.h
+++ Filas e pilhas II/main.h
@@ -1,17 +1,35 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 typedef struct
 {
     char expressao[200];
     int tamanho;
 } Expressao;
 
+/* Função que cria e retorna uma expressão vazia */
 Expressao criar_expressao();
-void apagar_expressao(Expressao *expressao);
-void imprimir_expressao(const Expressao *expressao);
-void adicionar_elemento(Expressao *expressao, const char *elemento);
-void converter_posfixa_para_prefixa(const Expressao *expressao_posfixa, Expressao *expressao_prefixa);
-float avaliar_expressao(const Expressao *expressao);
 
-#endif
+/* Função que apaga o conteúdo de uma expressão, deixando-a vazia */
+void apagar_expressao(Expressao *expressao);
+
+/* Função que imprime o conteúdo de uma expressão */
+void imprimir_expressao(const Expressao *expressao);
+
+/* Função que adiciona um elemento (número ou operador) a uma expressão existente */
+void adicionar_elemento(Expressao *expressao, const char *elemento);
+
+/* Função que avalia o resultado de uma expressão matemática */
+float resultado_expressao(const Expressao *expressao);
+
+/* Função que verifica se um caractere é um operador matemático */
+int isOperador(char c);
+
+/* Função que retorna a prioridade de um operador matemático*/
+int getPrioridade(char c);
+
+/* Função que inverte a ordem dos caracteres em uma string */
+void inverter_string(char *str);
+
+/* Função que converte uma expressão da notação posfixa para a notação prefixa */
+void converter_posfixa_para_prefixa(const Expressao *expressao_posfixa, Expressao *expressao_prefixa);
+
+/* Função que converte uma expressão da notação prefixa para a notação posfixa */
+void converter_prefixa_para_posfixa(const Expressao *expressao_prefixa, Expressao *expressao_posfixa);
