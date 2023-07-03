61bb116429d738a9e462833b7a4dccf51286ba37
h4rad4 <williamharad@outlook.com>
2023-07-02 21:27:05 -0400

    .

--- Deque/main.c
+++ Deque/main.c
@@ -7,14 +7,13 @@ int main()
     Deque turma;
     inicializar(&turma);
 
-    Aluno lista01[3] = {{"Polaris"}, {"Vega"}, {"Aldebaran"}};
-    Aluno lista02[3] = {{"Centauri"}, {"Lyra"}, {"Antares"}};
+    Aluno lista[6] = {{"Polaris"}, {"Vega"}, {"Aldebaran"}, {"Centauri"}, {"Lyra"}, {"Antares"}};
 
     for (int i = 0; i < 3; i++)
-        inserirInicio(&turma, lista01[i]);
+        inserirInicio(&turma, lista[i]);
 
-    for (int i = 0; i < 3; i++)
-        inserirFim(&turma, lista02[i]);
+    for (int i = 3; i < 6; i++)
+        inserirFim(&turma, lista[i]);
 
     imprimir(turma, 'd', 0);
     imprimir(turma, 'i', 5);
