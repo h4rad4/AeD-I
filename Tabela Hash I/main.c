edcce968be1f88d6dd5bbfec324d38e07a1d35f2
h4rad4 <williamharad@outlook.com>
2023-07-07 20:56:00 -0400

    .

---
+++ Tabela Hash I/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main()
+{
+    TB_HASH Tabela;
+    Paciente p1, p2, p3, p4, p5;
+
+    Paciente Pacientes[N] =
+        {{"Socrates", "209.478.341-42", 2470, 134201},
+         {"Platao", "754.912.563-29", 2448, 258902},
+         {"Aristoteles", "621.830.957-10", 2407, 405603},
+         {"Tales de Mileto", "485.739.620-83", 2600, 708904},
+         {"Pitagoras", "162.074.839-57", 2590, 126705}};
+
+    inicializarHash(Tabela);
+
+    for (int i = 0; i < N; i++)
+        inserirHash(Tabela, Pacientes[i]);
+
+    imprimirTabela(Tabela);
+
+    return 0;
+}
+
+/*
+    Exercícios
+
+    1. Implementar um TDA para uma tabela hash de pacientes
+    (codigo do paciente, nome, cpf, idade ) usando uma arrary  ponteiros a pacientes.
+
+    2. Implementar um TDA para uma tabela hash de pacientes
+    (codigo do paciente, nome, cpf, idade ) usando listas ligadas.
+
+*/
