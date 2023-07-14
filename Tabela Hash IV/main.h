db7462dfc75a4b8fc42def413fcdae1e30b06b0a
William Massami <99924347+h4rad4@users.noreply.github.com>
2023-07-14 13:36:26 -0400

    ✅

---
+++ Tabela Hash IV/main.h
@@ -0,0 +1,24 @@
+#ifndef ex1_h
+#define ex1_h
+#define N 10
+
+typedef struct
+{
+    int valor;
+} REGISTRO;
+
+typedef struct node
+{
+    REGISTRO reg;
+    struct node *next;
+} NODE;
+
+typedef NODE *TB_HASH[N];
+
+NODE *criarNo(int valor);
+int funcaoHash(int valor);
+void inserir(TB_HASH tabelaHash, int valor);
+void exibirTabelaHash(TB_HASH tabelaHash);
+void inicializarHash(TB_HASH tabela);
+
+#endif
