33edf9196e04611bb78bd8059ab86be3569e431d
h4rad4 <williamharad@outlook.com>
2023-07-31 00:58:34 -0400

    ✅

--- Árvore binária II/main.h
+++ Árvore binária II/main.h
@@ -27,15 +27,15 @@ typedef struct Pilha
 
 Pilha *criar_pilha();
 void empilhar(Pilha *pilha, NoArvore *dado);
+bool vazia(Pilha *pilha);
 NoArvore *desempilhar(Pilha *pilha);
 NoArvore *topo(Pilha *pilha);
-bool vazia(Pilha *pilha);
-int prioridade(char op);
-void inserir(Arvore *a, char dado);
-void infixa_para_posfixa(char *infixa, char *posfixa);
 NoArvore *criar_no(char dado);
 NoArvore *construir_arvore(char *posfixa);
+void inserir(Arvore *a, char dado);
 void liberar_arvore(NoArvore *no);
-int avaliar(NoArvore *no);
-void inverter(char *str);
+int prioridade(char op);
 void infixa_para_prefixa(char *infixa, char *prefixa);
+void infixa_para_posfixa(char *infixa, char *posfixa);
+void inverter(char *str);
+int avaliar(NoArvore *no);
